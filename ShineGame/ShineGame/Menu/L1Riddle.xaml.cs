0d68122bf8cf86bdb7a3a5dd33c617968c15668d
Lucas <lucaskern8@gmail.com>
2015-10-26 19:13:22 -0400

    Initial Commit

---
+++ ShineGame/ShineGame/Menu/L1Riddle.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPFPageSwitch.Menu
+{
+    /// <summary>
+    /// Interaction logic for L1Riddle.xaml
+    /// </summary>
+    public partial class L1Riddle : UserControl
+    {
+       DispatcherTimer t1 = new DispatcherTimer();
+
+        public L1Riddle()
+        {
+            t1.Interval = TimeSpan.FromSeconds(3);
+            t1.Tick += Timer_Tick;
+            t1.Start();
+
+            this.InitializeComponent();
+        }
+
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            Switcher.Switch(new Level1());
+            t1.Stop();
+        }
+    }
+}
