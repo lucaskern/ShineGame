5dd5524faf6b4c3f436480ec54139cd54acbf8b7
Lucas Russell Kern <lucaskern8@gmail.com>
2015-11-17 20:54:00 -0500

    Added Shine V2

---
+++ Shine2/Shine2/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Shine2.Pages;
+
+namespace Shine2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Switcher.pageSwitcher = this;
+            Switcher.Switch(new Shine2.Pages.Menu());      
+        }
+
+        public void Navigate(UserControl nextPage)
+        {
+            this.Content = nextPage;
+        }
+    }
+}
