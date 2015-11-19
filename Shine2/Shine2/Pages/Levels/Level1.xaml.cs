cf0ad8db977e54c1e96a2f4aef638cf1dba26c81
Lucas Russell Kern <lucaskern8@gmail.com>
2015-11-19 12:26:21 -0500

    Improved level 1, added inventory, added grab sound

--- Shine2/Shine2/Pages/Levels/Level1.xaml.cs
+++ Shine2/Shine2/Pages/Levels/Level1.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Shine2.Pages.Levels
 {
     /// <summary>
@@ -22,23 +23,30 @@ namespace Shine2.Pages.Levels
         //bool used to determine if the player has won (unused)
         //bool win = false;
         bool hasKey = false;
+        bool invShow = false;
         MediaPlayer media = new MediaPlayer();
+
+       
 
         public Level1()
         {
             InitializeComponent();
         }
 
-        //set win state to true, play tone, open win context menu
+        //Click key, add to inv and make hidden
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // win = true;
-            //media.Open(new Uri(@"../../Assets/Sound/solved2.m4a", UriKind.Relative));
-            //media.Play();
+            //play grab sound
+            media.Open(new Uri(@"../../Assets/Sound/grab2.m4a", UriKind.Relative));
+            media.Play();
 
+            //Hide key, register as holding
             key.Visibility = System.Windows.Visibility.Hidden;
+            hasKey = true;
 
-            //winBox.Visibility = System.Windows.Visibility.Visible;
+            //add key to inventory text
+            items.Text = "";
+            items.Text = items.Text + "Key";
         }
 
         //Go to menu
@@ -53,6 +61,39 @@ namespace Shine2.Pages.Levels
         {
             media.Stop();
             Switcher.Switch(new Level2Riddle());
+        }
+
+        // Drawer Area Click
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            //Only work when the player had picked up the key
+            if(hasKey == false)
+            {
+
+            } else if (hasKey == true)
+            {
+                //open the win menu and play tune
+                winBox.Visibility = System.Windows.Visibility.Visible;
+             
+                media.Open(new Uri(@"../../Assets/Sound/solved2.m4a", UriKind.Relative));
+                media.Play();
+            }
+        }
+
+        //Inventory open and close toggle
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+          
+            if (invShow == false)
+            {
+                inv.Visibility = System.Windows.Visibility.Visible;
+                invShow = true;
+            }
+            else if (invShow == true)
+            {
+                inv.Visibility = System.Windows.Visibility.Hidden;
+                invShow = false;
+            }
         }
     }
 }
